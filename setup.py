d6d0b0e3f7ff9d58bd6d3eb62122704ccffbfe36
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-05-16 20:39:49 -0700

    Initial layout (#1)
    * initial commit
    
    * update
    
    * update

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
+import os
+import os.path as osp
+import re
 from setuptools import find_packages, setup
+from typing import List, Optional
 
 __version__ = '0.1.0'
+
+
+def package_files(
+    root: str,
+    whitelist: Optional[List[str]] = None,
+) -> List[str]:
+    pattern = f'.({"|".join(whitelist or [])})$'
+
+    paths: List[str] = []
+    for path, _, names in os.walk(root):
+        for name in names:
+            if whitelist is not None and not re.search(pattern, name):
+                continue
+            paths.append(osp.join('..', path, name))
+    return paths
+
+
+print(package_files('pyg_sphinx_theme/static'))
 
 setup(
     name='pyg_sphinx_theme',
@@ -12,9 +34,12 @@ setup(
         'sphinx',
         'sphinx_rtd_theme',
     ],
-    package_data={'pyg_sphinx_theme': [
-        'theme.conf',
-    ]},
+    package_data={
+        'pyg_sphinx_theme': [
+            'theme.conf',
+            *package_files('pyg_sphinx_theme/static', ['css', 'png', 'svg']),
+        ]
+    },
     entry_points={
         'sphinx.html_themes': [
             'pyg_sphinx_theme = pyg_sphinx_theme',
