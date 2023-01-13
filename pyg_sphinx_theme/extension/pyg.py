d774ec8733d6b9facb27bdff29994f65c374e8cc
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-01-13 20:51:28 +0100

    Bundle `*.js` (#10)

--- pyg_sphinx_theme/extension/pyg.py
+++ pyg_sphinx_theme/extension/pyg.py
@@ -3,7 +3,7 @@ from pyg_sphinx_theme.extension.slack import SlackButton
 
 def setup(app):
     app.add_directive('slack', SlackButton)
-    app.add_js_file('js/onload.js')
+    app.add_js_file('js/on_pyg_load.js')
 
     return {
         'parallel_read_safe': True,
