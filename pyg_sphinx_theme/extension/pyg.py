23db8d4d82dc53cbe3a80c504a615eb7b5efffc6
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-13 20:33:05 +0100

    update

--- pyg_sphinx_theme/extension/pyg.py
+++ pyg_sphinx_theme/extension/pyg.py
@@ -3,6 +3,7 @@ from pyg_sphinx_theme.extension.slack import SlackButton
 
 def setup(app):
     app.add_directive('slack', SlackButton)
+    app.add_js_file('js/onload.js')
 
     return {
         'parallel_read_safe': True,
