3784be67c4c1bf2093b2ad03e3f90c24f0fbd1cc
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-01-14 15:21:23 +0100

    Added logo roles (#12)

--- pyg_sphinx_theme/extension/pyg.py
+++ pyg_sphinx_theme/extension/pyg.py
@@ -1,8 +1,13 @@
+from pyg_sphinx_theme.extension.logo import logo_role
 from pyg_sphinx_theme.extension.slack import SlackButton
 
 
 def setup(app):
     app.add_directive('slack', SlackButton)
+
+    app.add_role('pyg', logo_role)
+    app.add_role('pytorch', logo_role)
+
     app.add_js_file('js/on_pyg_load.js')
 
     return {
