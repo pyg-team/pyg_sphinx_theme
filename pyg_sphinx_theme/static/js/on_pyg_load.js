b651800f2b3677295dbbfb3f978154d3985a1b26
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-01-14 08:47:22 +0100

    Mac Dots Updates (#11)

--- pyg_sphinx_theme/static/js/on_pyg_load.js
+++ pyg_sphinx_theme/static/js/on_pyg_load.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
   while (elem.nextSibling)
     elem.nextSibling.remove();
 
-  // Copy text in code blocks:
+  // Code block header:
   var $highlight = $("div.rst-content div.highlight");
-  $highlight.append("<span class='copy fa fa-clipboard' title='Copy to clipboard'/>");
-  $highlight.click(function() {
-    navigator.clipboard.writeText($(this).parent().text().slice(0, -2));
+  $highlight.prepend("<div class='mac-header'><span class='dot red'></span><span class='dot yellow'></span><span class='dot green'></span><span class='copy fa fa-clipboard' title='Copy to clipboard'></span></div>");
+
+  // Copy text in code blocks:
+  $highlight.find('span.copy').click(function() {
+    navigator.clipboard.writeText($(this).parent().parent().text().slice(0, -1));
   });
 });
