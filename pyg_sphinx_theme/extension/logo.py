b7aa4301e3f5a6225a2ad21cb51b2bd9dd86ab93
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-14 15:29:37 +0100

    fix

--- pyg_sphinx_theme/extension/logo.py
+++ pyg_sphinx_theme/extension/logo.py
@@ -2,7 +2,7 @@ from docutils import nodes
 
 
 def logo_role(name, rawtext, text, *args, **kwargs):
-    node = nodes.inline(text=text)
+    node = nodes.inline(text=text if text != 'null' else '')
     node.set_class('inline-logo')
     node.set_class(name)
     return [node], []
