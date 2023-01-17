f643460697d6be04bc9d5e0668ba68b340628a45
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-17 04:55:09 +0100

    fix deprecation warning

--- pyg_sphinx_theme/extension/logo.py
+++ pyg_sphinx_theme/extension/logo.py
@@ -3,6 +3,5 @@ from docutils import nodes
 
 def logo_role(name, rawtext, text, *args, **kwargs):
     node = nodes.inline(text=text if text != 'null' else '')
-    node.set_class('inline-logo')
-    node.set_class(name)
+    node['classes'] += ['inline-logo', name]
     return [node], []
