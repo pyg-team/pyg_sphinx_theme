0c16804387939579b2284ea61703a141a4be3577
rusty1s <matthias.fey@tu-dortmund.de>
2023-02-19 13:27:21 +0100

    Handling empty text in logo

--- pyg_sphinx_theme/extension/logo.py
+++ pyg_sphinx_theme/extension/logo.py
@@ -4,4 +4,6 @@ from docutils import nodes
 def logo_role(name, rawtext, text, *args, **kwargs):
     node = nodes.inline(text=text if text != 'null' else '')
     node['classes'] += ['inline-logo', name]
+    if text == 'null':
+        node['classes'].append('empty')
     return [node], []
